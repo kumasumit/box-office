7ac5f1bc39067a2747e9efce04a57fd7146f70b0
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-03 11:49:04 +0530

    replaced useSate with useReducer on the Details Page

--- src/components/pages/Detail.jsx
+++ src/components/pages/Detail.jsx
@@ -1,26 +1,46 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable no-console */
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../../api-config/config';
 
 const Detail = () => {
   const { id } = useParams();
-  const [detail, setDetail] = useState(null);
-  const [isLoading, setisLoading] = useState(true);
-  const [error, setError] = useState(null);
+  // const [detail, setDetail] = useState(null);
+  // const [isLoading, setisLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  const reducer = (prevState, action) => {
+    switch (action.type) {
+      case 'FETCH_SUCCESS':
+        return { ...prevState, detal: action.detail, isLoading: false };
 
+      case 'FETCH_FAILED':
+        return { ...prevState, isLoading: false, error: action.error };
+
+      default:
+        return prevState;
+    }
+  };
+  const initialState = {
+    detail: null,
+    isLoading: true,
+    error: null,
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
   useEffect(() => {
     let isMounted = true;
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(results => {
         if (isMounted) {
-          setDetail(results);
-          setisLoading(false);
+          dispatch({ type: 'FETCH_SUCCESS', detail: results });
+          // setDetail(results);
+          // setisLoading(false);
         }
       })
       .catch(err => {
         if (isMounted) {
-          setError(err.message);
-          setisLoading(false);
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+          // setError(err.message);
+          // setisLoading(false);
         }
       });
     return () => {
@@ -28,13 +48,11 @@ const Detail = () => {
     };
   }, [id]);
 
-  console.log('detail', detail);
-
-  if (isLoading) {
+  if (state.isLoading) {
     return <div>Data is being loaded.</div>;
   }
-  if (error) {
-    return <div>Error ocurred : {error}.</div>;
+  if (state.error) {
+    return <div>Error ocurred : {state.error}.</div>;
   }
 
   return <div>This is the details page.</div>;
