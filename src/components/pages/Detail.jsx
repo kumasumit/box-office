45f0937c1734ce1552787abfddc5f5ce7bda02c8
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-03 10:30:04 +0530

    added loading and error messages to details page

--- src/components/pages/Detail.jsx
+++ src/components/pages/Detail.jsx
@@ -5,14 +5,37 @@ import { apiGet } from '../../api-config/config';
 const Detail = () => {
   const { id } = useParams();
   const [detail, setDetail] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setDetail(results);
-    });
+    let isMounted = true;
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setDetail(results);
+          setisLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setisLoading(false);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   console.log('detail', detail);
+
+  if (isLoading) {
+    return <div>Data is being loaded.</div>;
+  }
+  if (error) {
+    return <div>Error ocurred : {error}.</div>;
+  }
 
   return <div>This is the details page.</div>;
 };
